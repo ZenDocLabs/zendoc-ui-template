d03fb62aac534fe31c15e790b27eb6643030d547
dterbah <dterbah@macbookpro.home>
2025-04-11 14:48:48 +0200

    feat: Add loading of documentation according to the choosen version

--- src/context/DocContext.tsx
+++ src/context/DocContext.tsx
@@ -1,12 +1,12 @@
-import { createContext, ReactNode, useContext } from "react";
-import doc from "../assets/doc.json";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 import { ProjectDoc } from "../doc/doc";
+import { useDocVersion } from "./DocVersionContext";
 
 interface DocContextProps {
     doc: ProjectDoc;
 }
 
-const DocContext = createContext<DocContextProps>({ doc });
+const DocContext = createContext<DocContextProps>({ doc: {} as ProjectDoc });
 
 export const useDoc = () => {
     const context = useContext(DocContext);
@@ -14,6 +14,21 @@ export const useDoc = () => {
 }
 
 export const DocProvider = ({ children }: { children: ReactNode }) => {
+    const [doc, setDoc] = useState<ProjectDoc>();
+    const { version } = useDocVersion();
+    const allDocs = import.meta.glob("../assets/doc*.json");
+
+    useEffect(() => {
+        const loadDoc = async () => {
+            const file = allDocs[`../assets/doc-${version}.json`];
+            const currentDoc = await file() as any;
+            setDoc(currentDoc.default as ProjectDoc);
+        }
+        
+        loadDoc();
+    }, [version]);
+
+    if (!doc) return null;
     return (
         <DocContext.Provider value={{ doc }}>
             { children }
